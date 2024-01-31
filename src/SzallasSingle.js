b3a8a958ead0a1eac4619456c3af7fd1b1092715
sullaspqr <40822691+sullaspqr@users.noreply.github.com>
2024-01-31 13:30:05 +0100

    Neighbourhood javítva!

--- src/SzallasSingle.js
+++ src/SzallasSingle.js
@@ -37,7 +37,7 @@ export function SzallasSingle(props) {
         <div className="card w-100 m-auto p-3">
             <h1>{szallas.name}</h1>
             <h3>{szallas.host_name}</h3>
-            <h3>{szallas.neightbourhood} {szallas.neightbourhood_group}</h3>
+            <h3>{szallas.neighbourhood} {szallas.neighbourhood_group}</h3>
             <h3>{szallas.minimum_nights}</h3>
             <Kijelentkezes />
         </div>
